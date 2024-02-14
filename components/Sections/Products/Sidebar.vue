9bcd3dd5d00547252649d1229e102a750522169e
KamilOracz9 <kamiloracz9@gmail.com>
2024-02-14 12:22:11 +0100

    Fixing filters

--- components/Sections/Products/Sidebar.vue
+++ components/Sections/Products/Sidebar.vue
@@ -1,6 +1,6 @@
 <template>
     <section :class="productsFilterStore.isOpen ? '' : 'hidden'"
-        class="absolute p-5 top-0 left-0 bg-white w-full h-screen overflow-y-auto z-40 flex-col lg:h-fit lg:overflow-hidden lg:p-0 lg:!flex lg:relative lg:z-0 lg:w-[250px]">
+        class="fixed p-5 top-0 left-0 bg-white w-full h-screen overflow-y-auto z-40 flex-col pb-10 lg:pb-0 lg:h-fit lg:overflow-hidden lg:p-0 lg:!flex lg:relative lg:z-0 lg:w-[250px]">
         <div class="text-[1.25rem] flex justify-between lg:hidden">
             <span class="underline">{{ $t('filtering') }} / {{ $t('sorting') }}</span>
             <button class="lg:hidden" @click="productsFilterStore.toggleMenuIsOpen">
