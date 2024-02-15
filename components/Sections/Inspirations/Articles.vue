1aeeddfa71dad641370ae0b6fa0e70816475ab44
KamilOracz9 <kamiloracz9@gmail.com>
2024-02-15 14:10:12 +0100

    Adding animations to inspirations

--- components/Sections/Inspirations/Articles.vue
+++ components/Sections/Inspirations/Articles.vue
@@ -1,6 +1,6 @@
 <template>
     <ul class="gap-10 grid md:grid-cols-3" :class="props.class ? props.class : ''">
-        <SectionsInspirationsArticlesItem v-for="(article, index) in articles" :key="index" :article="article"
+        <SectionsInspirationsArticlesItem v-for="(article, index) in articles" :key="index" :article="article" data-aos="fade-up" :data-aos-delay="100 * index"
             :categorySlug="categorySlug" />
     </ul>
 </template>
