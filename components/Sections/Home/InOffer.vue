1ec6a4b6572b71cb10bed27bad8d7c87d0f1fc7b
KamilOracz9 <kamiloracz9@gmail.com>
2024-02-16 09:09:25 +0100

    Making pagination clickable

--- components/Sections/Home/InOffer.vue
+++ components/Sections/Home/InOffer.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="bg-gray-2 px-10 py-6 mt-7 h-fit sm:text-[1.25rem] sm:leading-6">
+    <section class="bg-gray-2 px-10 pb-6 mt-7 h-fit sm:text-[1.25rem] sm:leading-6">
         <h5 class="font-bold text-lg leading-6 mt-10 mb-2 sm:text-xl">{{ $t('home.in-offer.colors') }}</h5>
 
         <ul class="pl-8">
@@ -10,10 +10,10 @@
 
         <h5 data-aos="fade-up" class="font-bold text-lg leading-6 mt-10 mb-2 sm:text-xl">{{ $t('home.in-offer.variants') }}</h5>
 
-        <ul>
+        <ul class="grid gap-4">
             <li v-for="collection in homeStore.inOffer.collections" class="mb-6">
                 <NuxtLink data-aos="fade-up" class="flex gap-2 font-bold" :to="collection.url" :aria-label="collection.title"><Arrow :direction="'right'" /><h6>{{ collection.title }}</h6></NuxtLink>
-                <ul class="pl-8 mt-2">
+                <ul class="pl-8 mt-2 grid gap-2">
                     <li class="list-disc" v-for="(category, index) in collection.categories" data-aos="fade-up" :data-aos-delay="100 * index">
                         <NuxtLink :to="category.url" :aria-label="category.title">{{ category.title }}</NuxtLink>
                     </li>
