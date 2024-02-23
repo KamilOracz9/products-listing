a02cc508849116b5f7ff65c76db17d92cd1b25c3
KamilOracz9 <kamiloracz9@gmail.com>
2024-02-23 12:13:38 +0100

    Adding animations to contact page

--- components/Sections/Contact/ExportDepartment.vue
+++ components/Sections/Contact/ExportDepartment.vue
@@ -1,14 +1,14 @@
 <template>
     <section class="mt-10" :id="slugify($t('pages.contact.export-department'))">
-        <p class="section-subtitle">{{ $t('pages.contact.export-department') }}</p>
+        <p data-aos="fade-up" class="section-subtitle">{{ $t('pages.contact.export-department') }}</p>
 
-        <div class="section-text" v-html="exportDepartment.text"></div>
+        <div data-aos="fade-up" class="section-text" v-html="exportDepartment.text"></div>
 
         <div class="mt-6 md:grid md:grid-cols-5 lg:grid-cols-3 gap-6 lg:gap-10">
-            <div class="md:col-span-2 lg:col-span-1">
+            <div data-aos="fade-up" class="md:col-span-2 lg:col-span-1">
                 <img class="w-full" :src="exportDepartment.image" alt="">
             </div>
-            <div class="md:col-span-3 lg:col-span-2">
+            <div data-aos="fade-up" class="md:col-span-3 lg:col-span-2">
                 <SectionsContactEmployees :employees="exportDepartment.employees" />
             </div>
         </div>
