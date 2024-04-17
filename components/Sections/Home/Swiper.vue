c5bf2473e791b394098e8a15f1418e1c36ed0b41
KamilOracz9 <kamiloracz9@gmail.com>
2024-04-17 09:28:08 +0200

    Fixing slider

--- components/Sections/Home/Swiper.vue
+++ components/Sections/Home/Swiper.vue
@@ -7,7 +7,7 @@
         <SwiperSlide v-for="(slide, index) in data" :key="index">
             <NuxtLink :to="slide.path" class="relative"
                 :aria-current-value="slide.title ?? `New Trendy - slide-${index}`">
-                <div class="h-[582px] flex sm:h-[401px] lg:h-[612px] 2xl:h-[716px]">
+                <div class="h-[582px] flex relative after:w-full after:h-full after:absolute after:bg-[linear-gradient(90deg,_rgba(29,29,27,0.4)_0%,_rgba(29,29,27,0)_40%,_rgba(29,29,27,0)_100%)] sm:h-[401px] lg:h-[612px] 2xl:h-[716px]">
                     <!-- <picture v-if="slide.image" class="w-full">
                         <source media="(min-width:1024px)" :srcset="slide.fileUrls.lg">
                         <source media="(min-width:640px)" :srcset="slide.fileUrls.sm">
@@ -25,9 +25,9 @@
                     </video>
                 </div>
 
-                <div class="absolute bottom-4 left-4 text-white md:bottom-1 lg:bottom-36 lg:left-12">
-                    <p class="text-2xl">{{ slide.subtitle }}</p>
-                    <h2 v-if="slide.title" class="text-[2.5rem] font-medium leading-[3rem] uppercase">{{ slide.title }}
+                <div class="absolute bottom-4 left-4 text-white md:bottom-1 lg:bottom-32 lg:left-20">
+                    <p class="text-3xl">{{ slide.subtitle }}</p>
+                    <h2 v-if="slide.title" class="text-[2.5rem] font-medium leading-[3rem] uppercase xl:text-[50px] xl:leading-[60px]">{{ slide.title }}
                     </h2>
                 </div>
             </NuxtLink>
@@ -38,7 +38,7 @@
             alt="">
 
         <div
-            class="swiper-pagination hidden absolute bottom-3 text-white z-20 !left-1/2 !-translate-x-1/2 !w-[80%] border-t border-gray-1 pt-2 lg:flex lg:text-lg lg:!bottom-9 xl:!w-[85%]">
+            class="swiper-pagination hidden absolute bottom-3 text-white z-20 !left-1/2 !-translate-x-1/2 !w-[80%] border-t border-white pt-2 lg:flex lg:text-base lg:!bottom-7 xl:!w-[85%]">
         </div>
 
         <img class="right-4 main-slider-arrow main-slider-arrow-next block lg:right-14 xl:right-16 2xl:right-20"
