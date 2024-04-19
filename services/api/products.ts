bfc2441bc30ce502d4cf665c6f33ab44a1d9baeb
KamilOracz9 <kamiloracz9@gmail.com>
2024-04-19 13:19:00 +0200

    Fixing header navigation

--- services/api/products.ts
+++ services/api/products.ts
@@ -1,4 +1,4 @@
 import { getLocaleIso } from "~/utils"
 
-export const fetchProducts = async (query: any) => (await $fetch(`${useAppConfig().public.apiBase}/v1/${getLocaleIso.value}/products`, { query }));
+export const fetchProducts = async (query: any) => (await $fetch(`${useAppConfig().public.apiBase}/v1/${getLocaleIso.value}/products`, { query: Object.fromEntries(Object.entries(query).filter(([, value]) => value)) }));
 export const fetchProductPage = async (slug: any) => (await $fetch(`${useAppConfig().public.apiBase}/v1/${getLocaleIso.value}/products/${slug}`));
