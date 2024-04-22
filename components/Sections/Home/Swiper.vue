0414843ac45e9e0c267b97536c7c903995cac4ce
KamilOracz9 <kamiloracz9@gmail.com>
2024-04-22 12:41:27 +0200

    Fixing swiper

--- components/Sections/Home/Swiper.vue
+++ components/Sections/Home/Swiper.vue
@@ -1,49 +1,54 @@
 <template>
-    <Swiper :navigation="sliderConfig.navigation" :pagination="sliderConfig.pagination"
-        @slideChange="activeSlide = $event.realIndex" class="relative rounded-tr-lg"
-        :modules="[SwiperAutoplay, SwiperEffectCreative, SwiperNavigation, SwiperPagination]"
-        :slides-per-view="sliderConfig.slidesPerView" :loop="sliderConfig.loop" :effect="sliderConfig.effect"
-        :autoplay="sliderConfig.autoplay" :creative-effect="sliderConfig.creativeEffect">
-        <SwiperSlide v-for="(slide, index) in data" :key="index">
-            <NuxtLink :to="slide.path" class="relative"
-                :aria-current-value="slide.title ?? `New Trendy - slide-${index}`">
-                <div class="h-[582px] flex relative after:w-full after:h-full after:absolute after:bg-[linear-gradient(90deg,_rgba(29,29,27,0.4)_0%,_rgba(29,29,27,0)_40%,_rgba(29,29,27,0)_100%)] sm:h-[401px] lg:h-[612px] 2xl:h-[716px]">
-                    <!-- <picture v-if="slide.image" class="w-full">
+    <div>
+        <Swiper :navigation="sliderConfig.navigation" :pagination="sliderConfig.pagination"
+            @slideChange="activeSlide = $event.realIndex" class="relative rounded-tr-lg"
+            :modules="[SwiperAutoplay, SwiperEffectCreative, SwiperNavigation, SwiperPagination]"
+            :slides-per-view="sliderConfig.slidesPerView" :loop="sliderConfig.loop" :effect="sliderConfig.effect"
+            :autoplay="sliderConfig.autoplay" :creative-effect="sliderConfig.creativeEffect">
+            <SwiperSlide v-for="(slide, index) in data" :key="index">
+                <NuxtLink :to="slide.path" class="relative"
+                    :aria-current-value="slide.title ?? `New Trendy - slide-${index}`">
+                    <div
+                        class="h-[582px] flex relative after:w-full after:h-full after:absolute after:bg-[linear-gradient(90deg,_rgba(29,29,27,0.4)_0%,_rgba(29,29,27,0)_40%,_rgba(29,29,27,0)_100%)] sm:h-[401px] lg:h-[612px] 2xl:h-[716px]">
+                        <!-- <picture v-if="slide.image" class="w-full">
                         <source media="(min-width:1024px)" :srcset="slide.fileUrls.lg">
                         <source media="(min-width:640px)" :srcset="slide.fileUrls.sm">
                         <img preset="home-swiper" :src="slide.fileUrls.base" class="h-full w-full object-cover"
                             :alt="slide.title ?? `New Trendy - slide-${index}`"
                             :title="slide.title ?? `New Trendy - slide-${index}`" />
                     </picture> -->
-                    <img preset="home-swiper" :src="slide.image" class="h-full w-full object-cover"
-                        :alt="slide.title ?? `New Trendy - slide-${index}`"
-                        :title="slide.title ?? `New Trendy - slide-${index}`" />
+                        <img preset="home-swiper" :src="slide.image" class="h-full w-full object-cover"
+                            :alt="slide.title ?? `New Trendy - slide-${index}`"
+                            :title="slide.title ?? `New Trendy - slide-${index}`" />
 
-                    <video muted loop webkit-playsinline playsinline autoplay v-if="slide.video"
-                        class="h-full w-full object-cover">
-                        <source :src="slide.video" type="video/mp4">
-                    </video>
-                </div>
+                        <video muted loop webkit-playsinline playsinline autoplay v-if="slide.video"
+                            class="h-full w-full object-cover">
+                            <source :src="slide.video" type="video/mp4">
+                        </video>
+                    </div>
 
-                <div class="absolute bottom-4 left-4 text-white md:bottom-1 lg:bottom-32 lg:left-20">
-                    <p class="text-3xl">{{ slide.subtitle }}</p>
-                    <h2 v-if="slide.title" class="text-[2.5rem] font-medium leading-[3rem] uppercase xl:text-[50px] xl:leading-[60px]">{{ slide.title }}
-                    </h2>
-                </div>
-            </NuxtLink>
-        </SwiperSlide>
+                    <div class="absolute bottom-4 left-4 text-white md:bottom-1 lg:bottom-32 lg:left-20">
+                        <p class="text-3xl">{{ slide.subtitle }}</p>
+                        <h2 v-if="slide.title"
+                            class="text-[2.5rem] font-medium leading-[3rem] uppercase xl:text-[50px] xl:leading-[60px]">
+                            {{ slide.title }}
+                        </h2>
+                    </div>
+                </NuxtLink>
+            </SwiperSlide>
 
-        <img src="@/assets/icons/slider-arrow.svg"
-            class="left-4 main-slider-arrow main-slider-arrow-prev block rotate-180 lg:left-14 xl:left-16 2xl:left-20"
-            alt="">
+            <img src="@/assets/icons/slider-arrow.svg"
+                class="left-4 main-slider-arrow main-slider-arrow-prev block rotate-180 lg:left-14 xl:left-16 2xl:left-20"
+                alt="">
 
-        <div
-            class="swiper-pagination hidden absolute bottom-3 text-white z-20 !left-1/2 !-translate-x-1/2 !w-[80%] border-t border-white pt-2 lg:flex lg:text-base lg:!bottom-7 xl:!w-[85%]">
-        </div>
+            <div
+                class="swiper-pagination hidden absolute bottom-3 text-white z-20 !left-1/2 !-translate-x-1/2 !w-[80%] border-t border-white pt-2 lg:flex lg:text-base lg:!bottom-7 xl:!w-[85%]">
+            </div>
 
-        <img class="right-4 main-slider-arrow main-slider-arrow-next block lg:right-14 xl:right-16 2xl:right-20"
-            src="@/assets/icons/slider-arrow.svg" alt="">
-    </Swiper>
+            <img class="right-4 main-slider-arrow main-slider-arrow-next block lg:right-14 xl:right-16 2xl:right-20"
+                src="@/assets/icons/slider-arrow.svg" alt="">
+        </Swiper>
+    </div>
 </template>
 
 <script setup lang="ts">
