4fafdbabd589d9e03160b83cbdfbe9b738195244
KamilOracz9 <kamiloracz9@gmail.com>
2024-04-29 12:37:53 +0200

    Adding dimensions and lazy loading to footer icons

--- components/Footer/Contact.vue
+++ components/Footer/Contact.vue
@@ -3,21 +3,21 @@
         <p class="uppercase">{{ title }}</p>
         <!-- Addresses -->
         <div class="text-normal-base flex gap-3" v-if="addresses?.length"><img src="@/assets/icons/map-pin.svg"
-                class="footer__icon" alt="" />
+                class="footer__icon" width="16" height="16" alt="" />
             <div class="flex flex-col ">
                 <p class="footer__url" v-for="address in addresses">{{ address }}</p>
             </div>
         </div>
         <!-- Phones -->
         <div class="text-normal-base flex gap-3" v-if="phones?.length"><img src="@/assets/icons/phone.svg"
-                class="footer__icon" alt="" />
+                class="footer__icon" width="16" height="16" alt="" />
             <div class="flex flex-col">
                 <NuxtLink :to="`tel:${phone}`" class="footer__url" v-for="phone in phones">{{ phone }}</NuxtLink>
             </div>
         </div>
         <!-- Emails -->
         <div class="text-normal-base flex gap-3" v-if="emails?.length"><img src="@/assets/icons/envelope.svg"
-                class="footer__icon" alt="" />
+                class="footer__icon" width="16" height="16" alt="" />
             <div class="flex flex-col">
                 <NuxtLink :to="`mailto:${email}`" class="footer__url" v-for="email in emails">{{ email }}</NuxtLink>
             </div>
