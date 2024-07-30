e2da40ebb5ff129aaa45e966dbb9712da70e91bf
KamilOracz9 <kamiloracz9@gmail.com>
2024-07-30 07:41:38 +0200

    Changing hover for configurator button

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <NuxtLink to="https://konfiguratorkabin.pl" aria-label="konfiguratorkabin.pl"
-      class="flex text-[.7rem] mt-8 mb-7 xs:text-sm md:text-xl md:w-fit">
+      class="flex text-[.7rem] mt-8 mb-7 group xs:text-sm md:text-xl md:w-fit">
       <span
-        class="bg-black-2 text-white px-2 rounded-tl-xs hover:bg-yellow-2 transition-all xs:px-4 xs:py-2  md:rounded-tl-sm md:px-5 md:py-3">konfiguratorkabin.pl</span>
+        class="bg-black-2 text-white px-2 rounded-tl-xs group-hover:bg-yellow-2 transition-all xs:px-4 xs:py-2  md:rounded-tl-sm md:px-5 md:py-3">konfiguratorkabin.pl</span>
       <span class="px-2 uppercase font-medium xs:px-4 xs:py-2 md:px-5 md:py-3">{{ $t('made-to-measure-home-link')
         }}</span>
     </NuxtLink>
