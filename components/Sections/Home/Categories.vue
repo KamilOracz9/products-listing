a6f15756d79f2ea48344c9087364966833a82638
KamilOracz9 <kamiloracz9@gmail.com>
2024-09-16 13:47:02 +0200

    Adding hover animation to categories

--- components/Sections/Home/Categories.vue
+++ components/Sections/Home/Categories.vue
@@ -1,8 +1,9 @@
 <template>
     <section class="my-5 flex overflow-x-auto no-scrollbar">
-        <NuxtLink v-for="(category, index) in data" data-aos="fade-up" :aria-label="category.name"
-            :data-aos-delay="100 * index" :key="index"
-            class="border-l-2 border-black px-5 text-medium-lg uppercase flex items-center xs:pr-20 sm:pr-40 sm:text-xl lg:pr-5 lg:flex-1"
+        <NuxtLink v-for="(category, index) in data" data-aos="fade-up" :data-aos-delay="100 * index"
+            :aria-label="category.name" :key="index"
+            class="border-l-2 border-black px-5 text-medium-lg uppercase flex items-center aos-init aos-animate hover:text-gray-4 xs:pr-20 sm:pr-40 sm:text-xl lg:pr-5 lg:flex-1"
+            style="transition: all 150ms;"
             :to="category.path" v-html="category.name"></NuxtLink>
     </section>
 </template>
