450462c770f63d495563ef1b6d6eac68fe08fb51
KamilOracz9 <kamiloracz9@gmail.com>
2024-09-16 09:01:42 +0200

    Change building type

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,15 +1,15 @@
 module.exports = {
   apps: [
-    {
-      name: 'NewTrendy',
-      port: 3001,
-      script: "npx serve .output/public",
-      interpreter: "none",
-    }
     // {
     //   name: 'NewTrendy',
-    //   port: '3001',
-    //   script: 'node ./.output/server/index.mjs',
+    //   port: 3001,
+    //   script: "npx serve .output/public",
+    //   interpreter: "none",
     // }
+    {
+      name: 'NewTrendy',
+      port: '3000',
+      script: 'node .output/server/index.mjs',
+    }
   ]
 }
