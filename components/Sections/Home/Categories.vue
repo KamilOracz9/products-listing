5d19e3e9e8eab4f048d3da424be73eafddeac649
KamilOracz9 <kamiloracz9@gmail.com>
2024-09-18 12:02:41 +0200

    Changing home categories margin

--- components/Sections/Home/Categories.vue
+++ components/Sections/Home/Categories.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="my-5 flex overflow-x-auto no-scrollbar">
+    <section class="my-8 flex overflow-x-auto no-scrollbar">
         <NuxtLink v-for="(category, index) in data" data-aos="fade-up" :data-aos-delay="100 * index"
             :aria-label="category.name" :key="index"
             class="border-l-2 border-black px-5 text-medium-lg uppercase flex items-center aos-init aos-animate hover:text-gray-4 xs:pr-20 sm:pr-40 sm:text-xl lg:pr-5 lg:flex-1"
