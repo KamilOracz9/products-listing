9f80f1d5e4105d22c9744d43b72baec22b4d802e
Kamil <kamiloracz9@gmail.com>
2024-09-19 15:17:37 +0200

    Adding slug to listing items

--- components/Sections/Products/Listing.vue
+++ components/Sections/Products/Listing.vue
@@ -3,7 +3,7 @@
         <ul class="grid gap-8 xs:grid-cols-2 sm:grid-cols-3 lg:mt-10 xl:grid-cols-4 2xl:grid-cols-5">
             <li v-for="product in products" class="border-b border-gray-2 pb-8" data-aos="fade-in">
                 <NuxtLink :aria-label="product.name" :title="product.name"
-                    :to="localePath({ name: 'products' }) + `/${route.params.category}/${product.slug}`">
+                    :to="localePath({ name: 'products' }) + `/${route.params.category ?? product.category_slug}/${product.slug}`">
                     <figure>
                         <div class="relative">
                             <SectionsProductsBadge :badge="product.badge" />
