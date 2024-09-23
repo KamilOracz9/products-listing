abd2137b0a9ac4f3499a205f4f8c7151d9948bf8
KamilOracz9 <kamiloracz9@gmail.com>
2024-09-23 07:52:19 +0200

    Adding condition to swiper image display

--- components/Sections/Header/Logo.vue
+++ components/Sections/Header/Logo.vue
@@ -2,7 +2,7 @@
     <ul class="pr-5 flex justify-between items-center xl:mr-[70px]">
         <li>
             <NuxtLink :to="localePath({path: '/'})" :aria-label="$t('home-page')">
-                <img preload src="@/assets/images/logo.svg" class="aspect-[auto_4/3] w-[181px] small-height:w-[100px]" width="214" alt="" />
+                <NuxtImg preload src="logo.svg" class="aspect-[auto_4/3] w-[181px] small-height:w-[100px]" width="214" alt="" />
             </NuxtLink>
         </li>
 
