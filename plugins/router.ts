39c91a2e29e1d29f46983acae161a25cd3e14748
KamilOracz9 <kamiloracz9@gmail.com>
2025-01-15 15:02:44 +0100

    Fixing domains lang switcher

--- plugins/router.ts
+++ plugins/router.ts
@@ -28,14 +28,14 @@ export default defineNuxtPlugin(() => {
         //     return navigateTo(localePath(to.fullPath, 'cs').replace('/pl', ''))
         // }
 
-        // if (window?.location.host === 'newtrendy.eu') navigateTo(`https://newtrendy.eu${localePath(to.fullPath, 'en').replace('/pl', '')}`, { external: true, redirectCode: 301 });
-        if (window?.location.host === 'newtrendy.de') navigateTo(`https://newtrendy.eu${localePath(to.fullPath, 'de').replace('/pl', '')}`, { external: true, redirectCode: 301 });
-        if (window?.location.host === 'newtrendy.sk') navigateTo(`https://newtrendy.eu${localePath(to.fullPath, 'sk').replace('/pl', '')}`, { external: true, redirectCode: 301 });
-        if (window?.location.host === 'newtrendy.ua') navigateTo(`https://newtrendy.eu${localePath(to.fullPath, 'uk').replace('/pl', '')}`, { external: true, redirectCode: 301 });
-        if (window?.location.host === 'newtrendy.ro') navigateTo(`https://newtrendy.eu${localePath(to.fullPath, 'ro').replace('/pl', '')}`, { external: true, redirectCode: 301 });
-        if (window?.location.host === 'newtrendy.cz') navigateTo(`https://newtrendy.eu${localePath(to.fullPath, 'cs').replace('/pl', '')}`, { external: true, redirectCode: 301 });
-        if (window?.location.host === 'newtrendy.hu') navigateTo(`https://newtrendy.eu${localePath(to.fullPath, 'hu').replace('/pl', '')}`, { external: true, redirectCode: 301 });
-        if (window?.location.host === 'newtrendy.et') navigateTo(`https://newtrendy.eu${localePath(to.fullPath, 'et').replace('/pl', '')}`, { external: true, redirectCode: 301 });
+        if (useRequestURL().host === 'newtrendy.eu') navigateTo(`https://newtrendy.eu${localePath(to.fullPath, 'en').replace('/pl', '')}`, { external: true, redirectCode: 301 });
+        if (useRequestURL().host === 'newtrendy.de') navigateTo(`https://newtrendy.eu${localePath(to.fullPath, 'de').replace('/pl', '')}`, { external: true, redirectCode: 301 });
+        if (useRequestURL().host === 'newtrendy.sk') navigateTo(`https://newtrendy.eu${localePath(to.fullPath, 'sk').replace('/pl', '')}`, { external: true, redirectCode: 301 });
+        if (useRequestURL().host === 'newtrendy.ua') navigateTo(`https://newtrendy.eu${localePath(to.fullPath, 'uk').replace('/pl', '')}`, { external: true, redirectCode: 301 });
+        if (useRequestURL().host === 'newtrendy.ro') navigateTo(`https://newtrendy.eu${localePath(to.fullPath, 'ro').replace('/pl', '')}`, { external: true, redirectCode: 301 });
+        if (useRequestURL().host === 'newtrendy.cz') navigateTo(`https://newtrendy.eu${localePath(to.fullPath, 'cs').replace('/pl', '')}`, { external: true, redirectCode: 301 });
+        if (useRequestURL().host === 'newtrendy.hu') navigateTo(`https://newtrendy.eu${localePath(to.fullPath, 'hu').replace('/pl', '')}`, { external: true, redirectCode: 301 });
+        if (useRequestURL().host === 'newtrendy.et') navigateTo(`https://newtrendy.eu${localePath(to.fullPath, 'et').replace('/pl', '')}`, { external: true, redirectCode: 301 });
         // if (window?.location.host === 'localhost:3002') navigateTo(`http://localhost:3000${localePath(to.fullPath, 'de').replace('/pl', '')}`, { external: true, redirectCode: 301 });
 
         if (!['pl', 'sk'].includes(i18n.locale.value) && to.name?.split('___')[0] === 'service') {
