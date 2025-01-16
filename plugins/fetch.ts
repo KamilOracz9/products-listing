bcf5b7b405931eae74fb6d8f42b26e5ecd7b20b5
kamiloracz9 <kamiloracz9@gmail.com>
2025-01-16 10:43:01 +0100

    Adding x-url header to urls

---
+++ plugins/fetch.ts
@@ -0,0 +1,19 @@
+// plugins/fetch-middleware.js
+export default defineNuxtPlugin(() => {
+    const originalFetch = globalThis.$fetch
+
+    globalThis.$fetch = (url, options = {}) => {
+        options.headers = {
+            ...options.headers,
+            'X-Url': useRequestURL().host,
+        }
+
+        return originalFetch(url, options)
+            .then((response) => {
+                return response
+            })
+            .catch((error) => {
+                throw error
+            })
+    }
+})
