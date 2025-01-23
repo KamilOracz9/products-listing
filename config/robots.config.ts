a83d46ee441850a0f3caf0d574b032f27359e023
kamiloracz9 <kamiloracz9@gmail.com>
2025-01-23 08:30:21 +0100

    Blocking AmazonBot and ClaudeBot

--- config/robots.config.ts
+++ config/robots.config.ts
@@ -1,6 +1,13 @@
 export default [
     { UserAgent: 'Googlebot' },
     { Allow: '/' },
+
     { UserAgent: '*' },
+    { Disallow: '/' },
+
+    { UserAgent: 'Amazonbot' },
+    { Disallow: '/' },
+
+    { UserAgent: 'ClaudeBot' },
     { Disallow: '/' },
 ]
