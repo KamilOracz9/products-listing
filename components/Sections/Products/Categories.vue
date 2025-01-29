8c91c407bacd443cd522d7bd5a6a6393adc7aa2f
KamilOracz9 <kamiloracz9@gmail.com>
2025-01-29 13:56:27 +0100

    Changing categories url

--- components/Sections/Products/Categories.vue
+++ components/Sections/Products/Categories.vue
@@ -3,7 +3,7 @@
         <ul class="flex overflow-x-auto gap-3 no-scrollbar 3xl:gap-8">
             <li class="bg-gray-2 rounded-br-xs max-w-[45%] flex-[0_0_45%] p-4 sm:max-w-[33.3%] sm:flex-[0_0_33.3%] xl:max-w-auto xl:flex-1"
                 v-for="category in categories">
-                <NuxtLink :to="localePath({ name: 'products' }) + `/${category.slug}`"
+                <NuxtLink :to="localePath({ name: 'products-category', params: {'category': category.slug} })"
                     class="w-full h-full flex flex-col" :title="category.name" :aria-label="category.name">
                     <p class="text-gray-1 text-sm leading-[100%]">({{ category.count }})</p>
                     <div class="mx-auto px-3.5 py-4">
