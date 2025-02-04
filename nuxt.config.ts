2e841813ed9d908963384948813fe65ddf89acdf
kamiloracz9 <kamiloracz9@gmail.com>
2025-02-04 09:54:21 +0100

    Removing product page from prerender

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,6 +24,7 @@ export default defineNuxtConfig({
             && !route[0].includes('blog')
             && !route[0].includes('categories')
             && !route[0].includes('search')
+            && !route[0].includes('product')
         }).map(route => Object.entries(route[1]).map(lang => lang[0] === 'pl' ? lang[1] : `/${lang[0]}${lang[1]}`)).flat().forEach(element => {
           routes.add(element);
         });
