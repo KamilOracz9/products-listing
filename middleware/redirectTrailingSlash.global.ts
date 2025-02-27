67f63cf10c03542ba599b40dd31faa5dd78628a6
kamiloracz9 <kamiloracz9@gmail.com>
2025-02-27 09:09:13 +0100

    Fixing redirect from another sites

--- middleware/redirectTrailingSlash.global.ts
+++ middleware/redirectTrailingSlash.global.ts
@@ -1,5 +1,5 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-    if (!to.path.endsWith('/') && !to.path.includes('.') && to.name.split('__')[0] !== 'index') {
+    if (!to.path.endsWith('/') && !to.path.includes('.') && to.name.split('__')[0] !== 'index' && from.name) {
         const { path, query, hash } = to
         const nextRoute = { path: `${path}/`, query, hash }
 
