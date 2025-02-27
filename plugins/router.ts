4c397245fb60f3c4881b977c01f040f42196b585
kamiloracz9 <kamiloracz9@gmail.com>
2025-02-27 07:27:13 +0100

    Fixing inifinity redirect on pl

--- plugins/router.ts
+++ plugins/router.ts
@@ -4,11 +4,11 @@ export default defineNuxtPlugin(() => {
     const i18n = nuxt.$i18n;
 
     addRouteMiddleware('redirect-middleware', (to, from) => {
-        // if (useRequestURL().host === 'newtrendy.eu' && i18n.locale.value === 'pl') navigateTo(`https://newtrendy.eu${localePath(to.fullPath, 'en').replace('/pl', '')}`, { external: true, redirectCode: 301 });
+        if (useRequestURL().host === 'newtrendy.eu' && i18n.locale.value === 'pl') navigateTo(`https://newtrendy.eu${localePath(to.fullPath, 'en').replace('/pl', '')}`, { external: true, redirectCode: 301 });
 
-        // if (!['pl', 'sk'].includes(i18n.locale.value) && to.name?.split('___')[0] === 'service') {
-        //     return navigateTo(`/${i18n.locale.value}`);
-        // }
+        if (!['pl', 'sk'].includes(i18n.locale.value) && to.name?.split('___')[0] === 'service') {
+            return navigateTo(`/${i18n.locale.value}`);
+        }
     },
         { global: true }
     )
