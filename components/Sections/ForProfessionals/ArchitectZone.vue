7a763ea99977d6e49834d8b6e39ea17c6151110f
KamilOracz9 <kamiloracz9@gmail.com>
2025-10-24 08:49:34 +0200

    Add person name

--- components/Sections/ForProfessionals/ArchitectZone.vue
+++ components/Sections/ForProfessionals/ArchitectZone.vue
@@ -26,6 +26,9 @@
                 <div class="bg-yellow-2 p-8 rounded-tl-[25px] mt-6 text-lg lg:text-xl 2xl:w-[calc(50%-1.25rem)] 2xl:ml-auto"
                     data-aos="fade-up">
                     <ul class="flex flex-col gap-6" style="font-size: clamp(1rem, 3cqw, 1.25rem);">
+                        <div class="flex gap-4 items-center" v-if="section_2.left.person">
+                            <img class="w-[28px] aspect-[1/1]"src="@/assets/icons/person.svg" alt=""> {{ section_2.left.person }}
+                        </div>
                         <a class="flex gap-4 items-center" :href="`tel:${section_2.left.phone}`"><img class="w-[28px] aspect-[1/1]"
                                 src="@/assets/icons/phone.svg" alt=""> {{ section_2.left.phone }}</a>
                         <a class="flex gap-4 items-center" :href="`mailto:${section_2.left.email}`"><img class="w-[28px] aspect-[1/1]"
