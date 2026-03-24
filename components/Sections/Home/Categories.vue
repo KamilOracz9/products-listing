7183ea9482f2c43f40eb1e77ce094b6109444c3c
kamiloracz9 <kamiloracz9@gmail.com>
2026-03-24 15:30:26 +0100

    Add cursor pointer to categories

--- components/Sections/Home/Categories.vue
+++ components/Sections/Home/Categories.vue
@@ -2,7 +2,7 @@
     <section class="my-8 flex overflow-x-auto no-scrollbar">
         <a v-for="(category, index) in data" data-aos="fade-up" :data-aos-delay="100 * index"
             :aria-label="category.name" :key="index"
-            class="border-l-2 border-black px-5 text-medium-lg uppercase flex w-max min-w-[220px] items-center aos-init aos-animate hover:text-gray-4 lg:pr-5 lg:flex-1"
+            class="border-l-2 border-black px-5 text-medium-lg uppercase flex w-max min-w-[220px] items-center aos-init aos-animate cursor-pointer hover:text-gray-4 lg:pr-5 lg:flex-1"
             style="transition: all 150ms;"
             v-on:click="router.push(category.path)" v-html="category.name"></a>
         <!-- <a v-for="(category, index) in data" data-aos="fade-up" :data-aos-delay="100 * index"
