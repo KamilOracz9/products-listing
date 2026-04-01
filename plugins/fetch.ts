220dfa290ea4cec3a95e3bbd6e5c28af152db0ed
KamilOracz9 <kamiloracz9@gmail.com>
2026-04-01 13:05:21 +0200

    Test

--- plugins/fetch.ts
+++ plugins/fetch.ts
@@ -2,19 +2,8 @@ export default defineNuxtPlugin(() => {
     const requestHost = useRequestURL().host
 
     globalThis.$fetch = $fetch.create({
-        onRequest({ options }) {
-            const existing = options.headers
-                ? Object.fromEntries(
-                    options.headers instanceof Headers
-                        ? (options.headers as Headers).entries()
-                        : Object.entries(options.headers as Record<string, string>)
-                )
-                : {};
-
-            options.headers = {
-                ...existing,
-                'X-Url': requestHost,
-            };
+        headers: {
+            'X-Url': requestHost,
         }
-    });
+    })
 })
